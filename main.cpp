85d0bfabb13072f18fbb6e06ad1726613a4a0f1a
Chaewon <cwb1207@snu.ac.kr>
2024-06-07 08:33:52 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,11 +9,18 @@ vector<int> vec;
 
 void func(int cnt) {
 
-
-
-
-
-
+    if(vec.size()==M){
+	for(int i=0; i<M; i++){
+	    cout << vec[i];
+        }
+        cout << endl;
+        return;
+    }
+    for(int i=start+1; i<=N; ++i){
+	vec.push_back(i);
+        func(i+1);
+	vec.pop_back();
+    }
 
 }
 
@@ -20,3 +28,4 @@ int main() {
    cin >> N >> M;
    func(0);
 }
+//한 학기간 고생하셨습니다:)
